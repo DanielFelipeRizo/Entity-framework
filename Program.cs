36f8a8c171f8d71820d90f877548483b251a3467
DanielFelipeRizo <75734254+DanielFelipeRizo@users.noreply.github.com>
2022-11-15 17:29:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,126 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+using proyectoef.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+});
+
+//end poins de tareas
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext)=>
+{
+    return  Results.Ok(dbContext.Tareas.Include(p=> p.Categoria));
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea)=>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();   
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea,[FromRoute] Guid id)=>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual!=null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+
+    }
+
+    return Results.NotFound();   
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+     var tareaActual = dbContext.Tareas.Find(id);
+
+     if(tareaActual!=null)
+     {
+         dbContext.Remove(tareaActual);
+         await dbContext.SaveChangesAsync();
+
+         return Results.Ok();
+     }
+
+     return Results.NotFound();
+});
+
+
+//end points de categorias
+app.MapGet("/api/categorias", async ([FromServices] TareasContext dbContext)=>
+{
+    return Results.Ok(dbContext.Categorias);
+});
+
+app.MapPost("/api/categorias", async ([FromServices] TareasContext dbContext, [FromBody] Categoria categoria)=>
+{
+
+    await dbContext.AddAsync(categoria);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/categorias/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Categoria categoria,[FromRoute] Guid id)=>
+{
+    var categoriaActual = dbContext.Categorias.Find(id);
+
+    if(categoriaActual!=null)
+    {
+        categoriaActual.Nombre = categoria.Nombre;
+        categoriaActual.Descripcion = categoria.Descripcion;
+        categoriaActual.Peso = categoria.Peso;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+
+    }
+
+    return Results.NotFound();   
+});
+
+app.MapDelete("/api/categorias/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+     var categoriaActual = dbContext.Categorias.Find(id);
+
+     if(categoriaActual!=null)
+     {
+         dbContext.Remove(categoriaActual);
+         await dbContext.SaveChangesAsync();
+
+         return Results.Ok();
+     }
+
+     return Results.NotFound();
+});
+
+app.Run();
